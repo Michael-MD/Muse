df44cd5028c7f834e70d6a5a3f1b464fb1b16913
Michael-MD <mdei0001@student.monash.edu>
2024-12-06 23:28:34 +1100

    implemented 2d uint32_t array dumping

--- include/debugging/dumping.h
+++ include/debugging/dumping.h
@@ -5,3 +5,4 @@ void dump_1d_arrayf32(float* data, size_t length, const char* filename);
 void dump_1d_arrayu32(uint32_t* data, size_t length, const char* filename);
 
 void dump_2d_arrayf32(float** data, const size_t length_rows, const size_t length_cols, const char* filename);
+void dump_2d_arrayu32(uint32_t** data, const size_t length_rows, const size_t length_cols, const char* filename);
--- src/debugging/dumping.c
+++ src/debugging/dumping.c
@@ -69,3 +69,27 @@ void dump_2d_arrayf32(float** data, const size_t length_rows, const size_t lengt
     printf("Array dumped to file: %s\n", filename);
 
 }
+
+void dump_2d_arrayu32(uint32_t** data, const size_t length_rows, const size_t length_cols, const char* filename) {
+
+    // Open the file for writing (binary mode, or text mode depending on preference)
+    FILE* file = fopen(filename, "w");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    // Write array contents to the file
+    for (size_t i = 0; i < length_rows; i++) {
+        for (size_t j = 0; j < length_cols; j++) {
+            fprintf(file, "%u\n", data[i][j]);
+
+        }
+    }
+
+    // Close the file
+    fclose(file);
+    printf("Array dumped to file: %s\n", filename);
+
+}
