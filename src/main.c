f7ad2b35017bf77a5c297c150ea9ee5b596a6fda
Michael-MD <mdei0001@student.monash.edu>
2024-12-08 12:33:22 +1100

    added argtable3 to project and getopt.h for windows compilation. OpenMP is now enabled by default also.

--- src/main.c
+++ src/main.c
@@ -18,18 +18,20 @@
 #define ms * 1e-3
 
 #include <dirent.h>
-
+#include <argtable3.h>
 
 
 int main() {
 
 
-    double sample_rate = 44100;
+
+
+    /*double sample_rate = 44100;
     unsigned long frames_per_buffer_sec = 2;
     char* dir_src = "C:/Users/61481/Documents/code/sample music/database/";
 
 
-    begin_live_recognition(sample_rate, frames_per_buffer_sec, dir_src);
+    begin_live_recognition(sample_rate, frames_per_buffer_sec, dir_src);*/
 
 
     return 0;
